9d788ebbb6a42f89bbe947414b92e9b6abd6a03c
beneslami <beneslami72@gmail.com>
2025-07-07 02:47:06 +0200

    imporvement

--- dispatcherCore/dispatchDB.cpp
+++ dispatcherCore/dispatchDB.cpp
@@ -1,6 +1,19 @@
 #include "pubsub.h"
+#include "../common/dmsgOp.h"
+#include "../common/comm_type.h"
+#include "../common/ipc_struct.h"
+#include "dispatchDB.h"
 #include <unordered_map>
+#include <assert.h>
 #include <iostream>
+#include <stdio.h>
+#include <unordered_map>
+#include <stdexcept>
+#include <assert.h>
+#include <algorithm>
+#include <arpa/inet.h>
+#include <stddef.h>
+#include "../Libs/tlv.h"
 
 std::unordered_map<uint32_t, publisherDBentry_t*> pubDB;                    // <msg_id, publisher instance>
 std::unordered_map<uint32_t, std::shared_ptr<subscriberDBentry_t>>subDB;    // <msg_id, subscriber database entry>
@@ -101,4 +114,66 @@ void dispatcherDbDisplay() {
             std::cout << "Subscriber ID : " << sub->subscriberId << std::endl;
         }
     }
+}
+
+bool dispatcherProcessSubscriberIpcSubscription(uint32_t sub_id, dmsg_t *dmsg) {
+    assert(dmsg->msgType == SUB_TO_DISPATCH);
+    assert(dmsg->subMsgType == SUB_MSG_IPC_CHANNEL_ADD);
+
+    auto it = subDB.find(sub_id);
+    if (it == subDB.end()) {
+        printf("%s() : Error : Subscriber with ID %u not found.\n", __FUNCTION__, sub_id);
+        return false;
+    }
+
+    auto SubEntry = it->second.get();
+
+    if (SubEntry->ipcType != IPC_TYPE_NONE) {
+        printf("Coordinator : Error: Subscriber [%s,%u] IPC Channel Already Exists\n", SubEntry->subName, SubEntry->subscriberId);
+        return false;
+    }
+
+    if (dmsg->tlvBufferSize == 0) {
+        std::cout << "Coordinator : Error: Subscriber IPC Channel TLV Contains no IPC Data\n";
+        return false;
+    }
+
+    char *tlv_buffer = (char *)dmsg->tlvBuffer;
+    size_t tlv_buffer_size = dmsg->tlvBufferSize;
+    uint8_t tlv_data_len = 0;
+    uint8_t tlv_type;
+    char *tlv_value;
+
+    ITERATE_TLV_BEGIN(tlv_buffer, tlv_type, tlv_data_len, tlv_value, tlv_buffer_size) {
+        switch (tlv_type) {
+            case TLV_IPC_NET_UDP_SKT: {
+                    uint32_t ip_addr = *(uint32_t *)(tlv_value);
+                    ip_addr = htonl(ip_addr);
+                    uint16_t port = *(uint16_t *)(tlv_value + 4);
+                    port = htons(port);
+                    printf("Coordinator : Subscriber [%s,%u] IPC Channel Add : IP Address %u, Port %u\n", SubEntry->subName, SubEntry->subscriberId, ip_addr, port);
+
+                    SubEntry->ipcType = IPC_TYPE_NETSKT;
+                    SubEntry->ipcStruct.netskt.ipAddr = ip_addr;
+                    SubEntry->ipcStruct.netskt.port = port;
+                    SubEntry->ipcStruct.netskt.transportType = IPPROTO_UDP;
+            }
+            break;
+        }
+    } ITERATE_TLV_END;
+    return true;
+}
+
+void pubSubDbDeleteSubscriber(std::shared_ptr<pubSubDBentry_t> SubEntry) {
+    /*for (auto it = pubSubDB.begin(); it != pubSubDB.end(); ++it) {
+        pubSubDBentry_t*pubEntry = it->second;
+        auto &subscribers = pubEntry->subscribers;
+        subscribers.erase(std::remove(subscribers.begin(), subscribers.end(), SubEntry), subscribers.end());
+        if (subscribers.empty()) {
+            delete pubEntry;
+            it = pubSubDB.erase(it);
+        } else {
+            ++it;
+        }
+    }*/
 }
