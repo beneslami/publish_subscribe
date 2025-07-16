1f8ab677f90ac3a8eb3d737735f36580f62959b8
beneslami <beneslami72@gmail.com>
2025-07-16 12:43:32 +0200

    OO implementation

--- clientLib/client.h
+++ clientLib/client.h
@@ -1,16 +1,41 @@
+#pragma once 
+
+
 #include "../common/comm_type.h"
 #include "../common/ipc_struct.h"
 #include "../common/dmsgOp.h"
 
-void dispatcherRegister(int sock_fd, const char *entitiy_name, msgType_t msg_type);
-void dispatcherUnregister(int sock_fd, uint32_t pub_id, msgType_t msg_type);
-int pubSubDispatchMsg(int sock_fd, dmsg_t *dmsg);
 
-/* below two APIs update pubDB only */
-void publisherPublish(int sock_fd, uint32_t pub_id, uint32_t msg_id);
-void publisherUnPublish(int sock_fd, uint32_t pub_id, uint32_t msg_id);
+class Client {
+    private:
+        ipcStruct_t *_ipcStruct;
+        int _sockfd;
+    protected:
+        Client(ipcStruct_t *ipc_struct);
+        virtual ~Client();
+    public:
+        int pubSubDispatchMsg(dmsg_t *dmsg);
+        void dispatcherRegister(const char *entitiy_name, msgType_t msg_type);
+        void dispatcherUnregister(uint32_t pub_id, msgType_t msg_type);
+        int receiveFrom(char *arg, size_t size);
+};
 
-/* below two APIs update subDB and pubSubDB */
-void subscriberSubscribe(int sock_fd, uint32_t pub_id, uint32_t msg_id);
-void subscriberUnSubscribe(int sock_fd, uint32_t pub_id, uint32_t msg_id);
-void subscriberSubscribeIpcChannel(int sock_fd, uint32_t sub_id, ipcType_t ipc_type, ipcStruct_t *ipc_struct);
+class PublisherClient : public Client {
+    public:
+        PublisherClient(ipcStruct_t * ipc_struct);
+        ~PublisherClient();
+        void publisherPublish(uint32_t pub_id, uint32_t msg_id);
+        void publisherUnPublish(uint32_t pub_id, uint32_t msg_id);
+};
+
+class SubscriberClient : public Client {
+    public:
+        SubscriberClient(ipcStruct_t * ipc_struct);
+        ~SubscriberClient();
+        void subscriberSubscribe(uint32_t pub_id, uint32_t msg_id);
+        void subscriberUnSubscribe(uint32_t pub_id, uint32_t msg_id);
+        void subscriberSubscribeIpcChannel(uint32_t sub_id, ipcType_t ipc_type, ipcStruct_t *ipc_struct);
+};
+
+
+
